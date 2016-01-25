5e2fb2922859d4d12c04e65b04de8922ca189e01
Arthur Wang <arthraim@gmail.com>
2016-01-25 19:22:11 +0800

    feat: order

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
     @target.user_id = session[:login_user_id]
     @current_user = User.find session[:login_user_id] if session[:login_user_id]
     if @current_user
-      @statuses = Status.where(wb_uid: @current_user.target.wb_uid).to_a
+      @statuses = Status.where(wb_uid: @current_user.target.wb_uid).order(posted_at: :desc).to_a
     end
   end
 end
