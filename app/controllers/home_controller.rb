5cc47b90cf10921965c48f0561a55f7cb1ca1e46
Arthur Wang <arthraim@gmail.com>
2016-01-25 19:06:49 +0800

    feat: presentation of statuses

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,11 @@
 
 class HomeController < ApplicationController
   def index
-    @current_user = User.find session[:login_user_id] if session[:login_user_id]
     @target = Target.new
     @target.user_id = session[:login_user_id]
+    @current_user = User.find session[:login_user_id] if session[:login_user_id]
+    if @current_user
+      @statuses = Status.where(wb_uid: @current_user.target.wb_uid).to_a
+    end
   end
 end
