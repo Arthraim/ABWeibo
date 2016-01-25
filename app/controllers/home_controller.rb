01424d335eb76dc42f2ff79d1ec03a4c2ad8e9ad
Arthur Wang <arthraim@gmail.com>
2016-01-25 18:21:36 +0800

    fix: user not login

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 
 class HomeController < ApplicationController
   def index
-    @current_user = User.find session[:login_user_id]
+    @current_user = User.find session[:login_user_id] if session[:login_user_id]
     @target = Target.new
     @target.user_id = session[:login_user_id]
   end
