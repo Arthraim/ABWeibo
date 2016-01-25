e900148123ec6b1e72a00ba481f14cd580d49d1f
Arthur Wang <arthraim@gmail.com>
2016-01-25 17:57:36 +0800

    feat: select target's statuses in home timeline

--- app/workers/fetch_weibo_worker.rb
+++ app/workers/fetch_weibo_worker.rb
@@ -9,8 +9,15 @@ class FetchWeiboWorker
     client = WeiboOAuth2::Client.new
     client.access_token = WeiboOAuth2::AccessToken.new client, target.user.wb_token
 
-    resp = client.statuses.home_timeline access_token: target.user.wb_token, count: 100
-    # see doc for meaning of params: http://open.weibo.com/wiki/2/statuses/user_timeline
-    printf resp.inspect
+    resp = client.statuses.home_timeline access_token: target.user.wb_token, count: 100, trim_user: 1
+    # see doc for meaning of params: http://open.weibo.com/wiki/2/statuses/home_timeline
+    return unless resp.present? && resp.statuses.present?
+    resp.statuses.inject(Array.new) do |array, status|
+      if status.uid.to_s == target.wb_uid
+        array.push({ id: status.id, text: status.text })
+      else
+        array
+      end
+    end
   end
 end
